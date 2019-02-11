c29a82d151d79346727127051a45006beefab6ab
Viktoria <vstory@gmail.com>
2019-02-11 14:15:20 +0200

    lib directory deleted

--- src/main/java/ru/javawebinar/topjava/Constants.java
+++
@@ -1,7 +0,0 @@
-package ru.javawebinar.topjava;
-
-import java.text.SimpleDateFormat;
-
-public final class Constants {
-    public static final SimpleDateFormat SMPL_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm");
-}
